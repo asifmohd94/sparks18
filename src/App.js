44109ee6ac8dd3ad2f4486bb8cdd4abb9df6dca4
asifmohd94 <59363614+asifmohd94@users.noreply.github.com>
2020-10-17 11:53:53 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
+function App() {
+  return (
+   <div id="main-div">
+<header>
+  <div id="head-div">
+<ul className="list">
+  <li>Home</li>
+  <li>My Portfolio</li>
+</ul>
+
+  </div>
+</header>
+
+<div id="left"></div>
+
+
+   </div>
+  );
+}
+
+export default App;
